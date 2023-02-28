aaa0e7b17d18e89c2926ded48296118b0c26f081
yumiazusa <yumiazusa@hotmail.com>
2023-02-28 16:50:24 +0800

    2nd

--- database/seeders/StudentsSeeder.php
+++ database/seeders/StudentsSeeder.php
@@ -3,7 +3,7 @@
  * @Author: yumiazusa yumiazusa@hotmail.com
  * @Date: 2023-02-28 15:59:11
  * @LastEditors: yumiazusa yumiazusa@hotmail.com
- * @LastEditTime: 2023-02-28 15:59:27
+ * @LastEditTime: 2023-02-28 16:37:46
  * @FilePath: /www/datamaker/database/seeders/StudentssSeeder.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,7 +12,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class StudentssSeeder extends Seeder
+class StudentsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
